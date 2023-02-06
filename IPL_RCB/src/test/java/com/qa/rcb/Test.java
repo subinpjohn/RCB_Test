d1a135ba38d21e14a35b5b35fcf5d359658f3972
SUBIN P JOHN <subinpjohn@SUBINs-MacBook-Air.local>
2023-02-06 18:07:08 +0530

    Inital commit with Maximu foreign player test case

---
+++ IPL_RCB/src/test/java/com/qa/rcb/Test.java
@@ -0,0 +1,37 @@
+package com.qa.rcb;
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.testng.Assert;
+
+public class Test {
+	
+@org.testng.annotations.Test
+  public void  rcbTest(){
+    JSONParser parser = new JSONParser();
+    int nonIndianPlayersCount = 0;
+
+    try {
+      Object obj = parser.parse(new FileReader(System.getProperty("user.dir")+"/src/test/resources/TeamRCB.json"));
+      JSONObject jsonObject = (JSONObject) obj;
+      JSONArray playersArray = (JSONArray) jsonObject.get("player");
+
+      //To take the count foreign players
+      for (Object player : playersArray) {
+        JSONObject playerObject = (JSONObject) player;
+        String country = (String) playerObject.get("country");
+        if (!country.equals("India")) {
+          nonIndianPlayersCount++;
+        }
+      }
+      
+      Assert.assertEquals(nonIndianPlayersCount, 4);
+      
+    } catch (IOException | ParseException e) {
+      e.printStackTrace();
+    }
+  }
+}
